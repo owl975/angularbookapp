816650686c5627aaa041e2c92a73819eab8c7c3e
mtgomez <owl975@gmail.com>
2015-08-26 09:16:29 -0700

    added routes and create

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-angular.module('bookApp', [])
+angular.module('bookApp', ['ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/bookIndex.html',
+        controller: 'BooksCtrl'
+      })
+      .when('/books/:id', {
+        templateUrl: 'partials/bookShow.html',
+        controller: 'BooksShowCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }])
+	
 	.controller('BooksCtrl', ['$scope', '$http', function($scope, $http){
 		$scope.books = [];
 		$scope.book = {};
@@ -18,4 +33,26 @@ angular.module('bookApp', [])
 
   			});
   		};
-	}]);
+
+  		$scope.updateBook = function(book){
+  			$http.put('http://daretodiscover.herokuapp.com/books/' + book.id, book).
+  				then(function(response) {
+   				book.editForm = false;
+
+  			});
+  		};
+	}])
+
+  	.controller('BooksShowCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  		$scope.book = "This is the book";
+  		var bookId = $routeParams.id;
+
+  		$http.get('http://daretodiscover.herokuapp.com/books/' + bookId).
+  			then(function(response) {
+   			$scope.books = response.data;
+
+  			});
+  	}]);
+
+
+
